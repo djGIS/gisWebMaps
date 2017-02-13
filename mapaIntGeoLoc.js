d264b1b21d5f72459fca6c292efac0c659082096
DJ <djclarke.speaight@gmail.com>
2017-02-13 13:44:56 -0300

    Update mapaIntGeoLoc.js

--- mapaIntGeoLoc.js
+++ mapaIntGeoLoc.js
@@ -1,4 +1,4 @@
-//funciones de geolocalización
+//funciones de geolocalización y geocodificación de direcciones
 var geoLocFuente = "";
 
 function geoLocControls() {
@@ -45,4 +45,94 @@ function geoLocUsuario(fuente) {
 	} else {
 		alert('La función de ubicación automático está soportada por su browser.');
 	}
+}
+
+function geocodificar(campo, direccion) {
+	//userLocationMarker.setMap(null);
+	//userLocationMarker.setTitle(direccion);
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address':direccion, 'region':'ar'}, function(results, status) {
+		document.getElementById(campo.id).geoLocStatus = status;
+		if (status == google.maps.GeocoderStatus.OK) {
+			
+			document.getElementById(campo.id).value = results[0].formatted_address;
+			var p = results[0].address_components.length;
+			while (p > 0) {
+				p--;
+				if (results[0].address_components[p].types[0] == 'country') {
+					var routeWP = (results[0].address_components[p - 2].long_name + ', ');
+					routeWP += (results[0].address_components[p - 1].long_name + ', '); 
+					routeWP += results[0].address_components[p].long_name;
+					p = 0;	
+				}	
+			}	
+			document.getElementById(campo.id).statsValue = routeWP;
+			//userLocationMarker.setPosition(results[0].geometry.location);
+			//userLocationMarker.setMap(map);
+			//map.setZoom(15);
+			//map.setCenter(results[0].geometry.location);
+		} else {
+			document.getElementById(campo.id).statsValue = null;
+			//alert("Lo sentimos, no se pudo resolver la dirección que ingresó.");
+		}
+	});
+}
+
+function geocodificarInv(campo, ubicacion) {
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'location': ubicacion}, function(results, status) {
+		document.getElementById(campo.id).geoLocStatus = status;
+		if (status == google.maps.GeocoderStatus.OK) {
+			
+			//document.getElementById(campo.id).value = results[0].formatted_address;
+			var p = results[0].address_components.length;
+			while (p > 0) {
+				p--;
+				if (results[0].address_components[p].types[0] == 'country') {
+					var routeWP = (results[0].address_components[p - 2].long_name + ', ');
+					routeWP += (results[0].address_components[p - 1].long_name + ', '); 
+					routeWP += results[0].address_components[p].long_name;
+					p = 0;	
+				}	
+			}	
+			document.getElementById(campo.id).statsValue = routeWP;
+			//userLocationMarker.setPosition(results[0].geometry.location);
+			//userLocationMarker.setMap(map);
+			//map.setZoom(15);
+			//map.setCenter(results[0].geometry.location);
+		} else {
+			document.getElementById(campo.id).statsValue = null;
+			//alert("Lo sentimos, no se pudo resolver la dirección que ingresó.");
+		}
+	});
+}
+
+function codeAddress(campo) {
+	var address = campo.value;
+	var userLocation = tratarLocationInput(address);
+		
+	if (userLocation.address != "") {
+		geocodificar(campo, userLocation.address);
+	} else if (userLocation.location != null) {
+		geocodificarInv(campo, userLocation.location);
+	}
+}
+
+function tratarLocationInput(dirInput) {
+	var dirResult = {
+		location: new google.maps.LatLng(),
+		address: ""
+	};
+	
+	if (dirInput.substring(0, 6) == 'LatLng') {
+		z = 6;
+		while (dirInput.substring(z, z + 1) != ',') {
+			z++;
+		}
+		dirResult.location = new google.maps.LatLng(Number(dirInput.substring(7, z)),Number(dirInput.substring(z + 2, dirInput.length - 1)));
+	} else {
+		dirResult.address = dirInput;
+	}
+	
+	return dirResult;
 }
